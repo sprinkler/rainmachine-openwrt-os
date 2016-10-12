3105932ef4976e42ab564f64534a4dc6bdb4a3bc
Nicu Pavel <npavel@mini-box.com>
2016-10-12 12:02:34 +0300

    Scripts: Remove pull requests when generating changelog

--- beta-sync-remote-repository.sh
+++ beta-sync-remote-repository.sh
@@ -11,9 +11,9 @@ d=$(date +%Y-%m-%d)
 dt=$(date)
 f="/mnt/hall/rainmachine-ar71xx-beta/packages/Changelog.txt"
 l_openwrt=$(git  log rainmachine.. --format=-%s)
-l_openwrtfeed=$(git --git-dir ../rainmachine-openwrt-feed/.git log master.. --format=-%s)
+l_openwrtfeed=$(git --git-dir ../rainmachine-openwrt-feed/.git log master..next --format=-%s)
 l_rainmachine=$(git --git-dir ../rainmachine/.git log master.. --format=-%s )
-l_rainmachine_webui=$(git --git-dir ../../rainmachine-ui/.git log master.. --format=-%s | grep -v "Merge pull request" )
+l_rainmachine_webui=$(git --git-dir ../../rainmachine-ui/.git log master..next --format=-%s | grep -v "Merge pull request" )
 
 echo "Build: $dt" > $f
 echo "OpenWRT OS Changes:" >> $f
