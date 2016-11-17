4968de8177cb3f79a59e3e4ebce447bc39111d85
Nicu Pavel <npavel@mini-box.com>
2016-11-17 11:35:57 +0200

    Update sync script

--- sync-remote-repository.sh
+++ sync-remote-repository.sh
@@ -5,18 +5,18 @@ rm -rf $UPDATE_PATH/packages/
 cp -a bin/ar71xx/packages $UPDATE_PATH
 
 echo "Syncing REV2 images..."
-rm -rf /mnt/hall/sprinkler2/os/openwrt-ar71xx-generic-rainmachine-jffs2-*
-cp -a bin/ar71xx/openwrt-ar71xx-generic-rainmachine-jffs2-* /mnt/hall/sprinkler2/os/
+rm -rf /mnt/hall/sprinkler2-2/os/openwrt-ar71xx-rev2-rainmachine-jffs2-*
+cp -a bin/ar71xx/openwrt-ar71xx-rev2-rainmachine-jffs2-* /mnt/hall/sprinkler2-2/os/
 
 echo "Adding changelog"
 d=$(date +%Y-%m-%d)
 dt=$(date)
-f="/mnt/hall/sprinkler2/os/Changelog-Daily-Build.txt"
+f="/mnt/hall/sprinkler2-2/os/Changelog-Daily-Build.txt"
 l_openwrt=$(git  log --since="1 day ago" --format=-%s)
 l_openwrtfeed=$(git --git-dir ../rainmachine-openwrt-feed/.git log --since="1 day ago" --format=-%s)
 l_rainmachine=$(git --git-dir ../rainmachine/.git log --since="1 day ago" --format=-%s )
 
-echo "Build: $dt" > $f
+echo "Build REV2: $dt" > $f
 echo "OpenWRT OS Changes:" >> $f
 echo "$l_openwrt" >> $f
 echo >> $f 
