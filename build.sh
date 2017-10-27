e9ebd39dd968143a4b3b233bbe0d536b80b75563
developer <developer@rainmachie2.mini-box.com>
2017-10-27 11:50:15 +0300

    OpenWrt build: build.sh can now build rev1 and then rev 2

--- build.sh
+++ build.sh
@@ -1,35 +1,137 @@
-#!/bin/sh
+#!/bin/bash
+if [ "$#" -lt 1 ]; then
+    echo "Use alpha, beta or release"
+    exit 1
+fi
+if [ "$1" != "alpha" ] && [ "$1" != "beta" ] && [ "$1" != "release" ] ; then
+    echo "Use alpha, beta or release"
+    exit 1
+fi
+
 GIT_DIR="../rainmachine-openwrt-feed/.git/"
-FEED_BRANCH="next-rev2-fix"
 
 check_branch() {
-
-    CURRENT_BRANCH=`git --git-dir=$GIT_DIR symbolic-ref -q --short HEAD`
-    echo "Checking Rainmachine Feed for the $FEED_BRANCH == $CURRENT_BRANCH"
-
-    if [ $FEED_BRANCH != $CURRENT_BRANCH ]
+    CURRENT_FEED_BRANCH=`git --git-dir=$GIT_DIR symbolic-ref -q --short HEAD`
+    echo "Checking Rainmachine Feed for the $FEED_BRANCH == $CURRENT_FEED_BRANCH"
+    if [ $FEED_BRANCH != $CURRENT_FEED_BRANCH ]
     then
-    echo "Error not in the correct branch: $FEED_BRANCH"
-    if [ $1 ]
-    then
-	echo "Previous attempt to switch branch failed. Aborting"
-	exit 2
+        echo "Error not in the correct branch: $FEED_BRANCH"
+        if [ $1 ]
+	then
+	    echo "Previous attempt to switch branch failed. Aborting"
+        exit 2
+        fi
+        echo "Switching to $FEED_BRANCH branch"
+        pushd $GIT_DIR/..
+        git checkout $FEED_BRANCH
+        popd
     fi
-    echo "Switching to $FEED_BRANCH branch"
-    pushd $GIT_DIR/..
-    git checkout $FEED_BRANCH
-    popd
-    fi
-
 }
 
-check_branch
-check_branch final
 
-echo Removing existing packages
-rm -rf bin/ar71xx
-echo Cleaning kernel
-make target/linux/clean
+BRANCHES="rainmachine next|rainmachine-rev2 next-rev2-fix"
 
-echo Building
-make -j4
+build_current_branch(){
+    CURRENT_BUILD_BRANCH=`git symbolic-ref -q --short HEAD`
+    IFS="|"
+    for tuple in $BRANCHES; do
+	BRANCH=$(echo $tuple | cut -d " " -f 1)
+	FEED_BRANCH=$(echo $tuple | cut -d " " -f 2)
+	if [ $CURRENT_BUILD_BRANCH == $BRANCH ]; then
+	    check_branch
+	    check_branch final
+	    echo Removing existing packages
+	    rm -rf bin/ar71xx
+	    echo Cleaning kernel
+	    make target/linux/clean
+	    echo Building
+	    make -j4
+	fi
+    done
+}
+
+build_all_branches(){
+    IFS="|"
+    for tuple in $BRANCHES; do
+	BRANCH=$(echo $tuple | cut -d " " -f 1)
+	FEED_BRANCH=$(echo $tuple | cut -d " " -f 2)
+	git checkout $BRANCH
+	check_branch
+	check_branch final
+        echo Removing existing packages
+        rm -rf bin/ar71xx
+        echo Cleaning kernel
+        make target/linux/clean
+        echo Building
+        make -j4
+	sync_remote_repository $1
+    done	    
+}
+
+sync_remote_repository(){
+    case $1 in
+	"release")
+	_sync_remote_repository "release"
+	_sync_remote_repository "beta"
+	_sync_remote_repository "alpha"
+	;;
+	"beta")
+	_sync_remote_repository "beta"
+	_sync_remote_repository "alpha"
+	;;
+	"alpha")
+	_sync_remote_repository "alpha"
+	;;
+    esac
+}
+
+_sync_remote_repository(){
+    . ./rainmachine-build.conf
+    UPDATE_PATH=$UPDATE_LOCATION_ROOT/${UPDATE_LOCATION_PREFIX}rainmachine-ar71xx${MODEL_SUFFIX}
+    if [ "$1" != "release" ]; then
+	UPDATE_PATH=$UPDATE_PATH"-$1"
+    fi
+    UPDATE_PATH=$UPDATE_PATH"/"
+    echo "Syncing $MODEL $1 packages to $UPDATE_PATH"
+    rm -rf $UPDATE_PATH/packages/
+    cp -a bin/ar71xx/packages $UPDATE_PATH
+
+    echo "Syncing $MODEL  images..."
+    if [ -z  $MODEL_SUFFIX ]; then
+	MODEL_SUFFIX="-generic"
+    fi
+    rm -rf $UPDATE_LOCATION_ROOT/$UPDATE_LOCATION_BIN/os/openwrt-ar71xx${MODEL_SUFFIX}-rainmachine-jffs2-*
+    cp -a bin/ar71xx/openwrt-ar71xx${MODEL_SUFFIX}-rainmachine-jffs2-* $UPDATE_LOCATION_ROOT/$UPDATE_LOCATION_BIN/os/
+    
+    echo "Adding changelog"
+    d=$(date +%Y-%m-%d)
+    dt=$(date)
+    f="$UPDATE_LOCATION_ROOT/$UPDATE_LOCATION_BIN/os/Changelog-Daily-Build.txt"
+    l_openwrt=$(git  log --since="1 day ago" --format=-%s)
+    l_openwrtfeed=$(git --git-dir ../rainmachine-openwrt-feed/.git log --since="1 day ago" --format=-%s)
+    l_rainmachine=$(git --git-dir ../rainmachine/.git log --since="1 day ago" --format=-%s )
+
+    echo "Build $MODEL2: $dt" > $f
+    echo "OpenWRT OS Changes:" >> $f
+    echo "$l_openwrt" >> $f
+    echo >> $f 
+    echo "OpenWRT RainMachine Feed Changes:" >> $f
+    echo "$l_openwrtfeed" >> $f
+    echo >> $f
+    #echo "Rainmachine App Changes:" >> $f
+    #echo "$l_rainmachine" >> $f
+}
+
+if [ "$#" -eq 1 ]; then
+    echo Building current branch
+    build_current_branch
+    sync_remote_repository $1    
+    exit 0
+fi
+
+if [ $2 == "all" ]; then
+    echo Build all branches
+    build_all_branches $1
+    exit 0
+fi
+
